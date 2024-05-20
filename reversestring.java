8ea47c262025c4de69d96b72f5bbc45992c9947d
Vishwa Jothi.A <154690465+VishwaJothi@users.noreply.github.com>
2024-05-20 14:23:28 +0530

    Add files via upload

---
+++ reversestring.java
@@ -0,0 +1,20 @@
+
+public class reversestring {
+    public static void main(String[] args) {
+        String str1 ="vishwajothi";
+        System.out.println(str1);
+        String temp="";
+        for(int i=0;i<=str1.length()-1;i++){
+            temp=str1.charAt(i)+temp;
+
+        }
+        System.out.println(temp);
+        
+    }   
+}
+       
+
+    
+
+
+
